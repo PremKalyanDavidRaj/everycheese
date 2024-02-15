e37ae6ae3916391b835d5606b301ff03d23293de
PremKalyanDavidRaj <PremKalyanDavidRaj.Gai@unh.edu>
2024-02-14 17:00:50 -0800

    Implement cheese list page, add navbar link

--- everycheese/users/urls.py
+++ everycheese/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from everycheese.users.views import (
     user_redirect_view,
@@ -11,4 +11,5 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
+    path('cheeses/', include('everycheese.cheeses.urls', namespace='cheeses')),
 ]
